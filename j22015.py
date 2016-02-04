a5d5c1f50a6ecd5de121300e0c4e23ce2b6ea63f
mrfabroa <e>
2016-02-04 12:58:30 -0500

    fully functional 15/15

--- j22015.py
+++ j22015.py
@@ -1,1 +1,33 @@
 __author__ = 'robuntu'
+import sys
+
+line = sys.stdin.readline()
+
+happy_count = 0
+sad_count = 0
+
+emoticon = ""
+i = 0
+
+while i < len(line):
+    if line[i] == ":":
+        if line[i:i+3] == ":-)":
+            happy_count += 1
+            i += 3
+        elif line[i:i+3] == ":-(":
+            sad_count += 1
+            i += 3
+        else:
+            i+=1
+    else:
+        i += 1
+
+if happy_count == 0 and sad_count == 0:
+    print "none"
+elif happy_count > sad_count:
+    print "happy"
+elif sad_count > happy_count:
+    print "sad"
+else:
+    print "unsure"
+
