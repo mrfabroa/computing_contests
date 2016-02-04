ddc63bc8d158f3bffc1215dd3301cf9ee96cdd62
mrfabroa <eric.fabroa@ycdsbk12.ca>
2016-02-04 17:46:11 -0500

    Added 2014 directory

---
+++ 2014/j12014.py
@@ -0,0 +1,18 @@
+__author__ = 'eric'
+
+import sys
+
+val1 = int(sys.stdin.readline())
+val2 = int(sys.stdin.readline())
+val3 = int(sys.stdin.readline())
+
+valsum = val1 + val2 + val3
+
+if valsum != 180:
+    print "Error"
+elif val1 == 60 and val2 == 60 and val3 == 60:
+    print "Equilateral"
+elif val1 == val2 or val1 == val3 or val2 == val3:
+    print "Isosceles"
+else:
+    print "Scalene"
